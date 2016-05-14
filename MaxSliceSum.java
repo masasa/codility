398fd1485bd21ea125cc21624ede30ca6d92cae6
Omri Masasa <masasa@gmail.com>
2016-05-14 14:29:29 +0300

    problem solved: MaxSliceSum

--- MaxSliceSum.java
+++ MaxSliceSum.java
@@ -1,15 +1,15 @@
 package codility;
 
 class MaxSliceSum {
-	
+
     public int solution(int[] A) {
         
-    	int n = A.length;
-    	int maxSum = 0;
+    	int n = A.length, maxSum;
     	int[] maxEnds = new int[n];
 
     	// the max sum that ends at index 0 is the element itself
     	maxEnds[0] = A[0];
+    	maxSum = maxEnds[0];
 
     	// finding the maximal slice
     	for(int i=1; i<n; i++){
